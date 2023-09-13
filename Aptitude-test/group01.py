e6e2537cfc0c16267d6f1b96aad14cf6b05eb2d8
Prithvi2310 <shahprithvi1505@gmail.com>
2023-09-14 04:17:46 +0530

    finished with ML

--- Aptitude-test/group01.py
+++ Aptitude-test/group01.py
@@ -1,5 +1,13 @@
 import numpy as np
 import pandas as pd
+import joblib
+import pickle
 
 # importing the dataset
-dataset = pd.read_csv('data/aptitude-test-group01.csv')
+#dataset = pd.read_csv('data/aptitude-test-group01.csv')
+filename = '../Aptitude-test/finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+input = [18,  0,  2, 80, 75, 85, 90, 80, 70]
+input = input.reshape(1,-1)
+pred = loaded_model.predict(input)
+print(pred)
