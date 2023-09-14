0d6a7375bd91f3d3a678afb32d141c50f945d7e7
Annarhysa Albert <annarhysa13@gmail.com>
2023-09-14 05:35:39 +0530

    Model_pickel

--- Aptitude-test/personality_test.py
+++ Aptitude-test/personality_test.py
@@ -5,9 +5,11 @@ import pickle
 
 # importing the dataset
 #dataset = pd.read_csv('data/aptitude-test-group01.csv')
-filename = './personality_model.joblib'
-loaded_model = pickle.load(open(filename, 'rb'))
-input = [18,  0,  2, 80, 75, 85, 90, 80, 70]
+Pkl_file = 'CareerWise/Aptitude-test/pickle_model'
+with open(Pkl_file, 'rb') as file:
+    classifier=pickle.load(file)
+
+input = np.array([18,  0,  2, 80, 75, 85, 90, 80, 70])
 input = input.reshape(1,-1)
-pred = loaded_model.predict(input)
+pred = classifier.predict(input)
 print(pred)
